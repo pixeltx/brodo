9045606e8b78016f39b1e13a0110ea624e1cb063
wyd <arvinradithya.0@gmail.com>
2024-11-17 11:14:20 +0700

    first commit

---
+++ app/Filament/Resources/MovieResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Movie;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Facades\Storage;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\MovieResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\MovieResource\RelationManagers;
+
+class MovieResource extends Resource
+{
+    protected static ?string $model = Movie::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')->required(),
+                Textarea::make('description'),
+                Textarea::make('synopsis'),
+                FileUpload::make('image')
+                    ->label('Movie Image')
+                    ->directory('movies/images') // Directory penyimpanan gambar
+                    ->image()
+                    ->maxSize(5096), // Batas ukuran gambar dalam KB
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('averageRating')->label('Average Rating')->sortable()->getStateUsing(fn ($record) => $record->averageRating()),
+                TextColumn::make('created_at')->date(),
+                // TextColumn::make('image_url')->label('Debug URL')->getStateUsing(fn ($record) => $record->image_url),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMovies::route('/'),
+            'create' => Pages\CreateMovie::route('/create'),
+            'edit' => Pages\EditMovie::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Movie.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'image', 'synopsis'];
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function averageRating()
+    {
+        return $this->ratings()->avg('rating');
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url($this->image);
+    }
+}
